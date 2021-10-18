94f7388563b6043551d049ec3f9a88a9bd5d587e
Daniel Felsky <dfelsky@gmail.com>
2021-10-18 19:56:55 -0400

    added master script for order of operations

---
+++ code/master_script.R
@@ -0,0 +1,12 @@
+## Order of operations
+
+# First, make sure to create the following directories in your local repo (at the same level as code/)
+# output/
+# paper/figures
+# paper/tables
+
+# Second, retrieve raw data files and unzip to:
+# input/
+
+source("code/normalize_mono_data.R")
+source("code/normalize_dlpfc_data.R")
